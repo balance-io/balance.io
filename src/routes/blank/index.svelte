6a20020d39031b6bca7aa77d64e03e1b5f0e730c
Dimitar Nestorov <8790386+dimitarnestorov@users.noreply.github.com>
2022-02-04 20:45:29 +0200

    Add blank page

---
+++ src/routes/blank/index.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+    <style>
+        body {
+            background: white;
+            color: #393939;
+        }
+
+        body > * {
+            display: none !important;
+        }
+
+        @media (prefers-color-scheme: dark) {
+            body {
+                background: black;
+                color: #ccc;
+            }
+        }
+    </style>
+</svelte:head>
