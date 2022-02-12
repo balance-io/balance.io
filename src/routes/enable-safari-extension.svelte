7f1ccc2b48241d91ac015b376a76de41a810ade7
Dimitar Nestorov <8790386+dimitarnestorov@users.noreply.github.com>
2022-02-12 18:50:22 +0200

    Add enable-safari-extension page

---
+++ src/routes/enable-safari-extension.svelte
@@ -0,0 +1,51 @@
+<h2>Set up Balance</h2> 
+
+<div class="step">
+    <img src="/img/steps/step-1.png" alt="Step 1: Tap aA" />
+</div>
+<div class="arrow">↓</div>
+<div class="step">
+    <img src="/img/steps/step-2.png" alt="Step 2: Tap Manage Extensions" />
+</div>
+<div class="arrow">↓</div>
+<div class="step">
+    <img src="/img/steps/step-3.png" alt="Step 3: Enable Balance" />
+</div>
+
+<div class="go-home">
+    <a href="/">❮ Go back to the home page</a>
+</div>
+
+<style>
+    h2 {
+        text-align: center;
+    }
+    .step {
+        margin: 0 auto;
+        padding: 10px 15%;
+        text-align: center;
+        > img {
+            width: 400px;
+            max-width: 100%;
+            box-sizing: border-box;
+            padding: 10px;
+            border-radius: 20px;
+            background-color: white;
+        }
+    }
+
+    .arrow {
+        text-align: center;
+    }
+
+    .go-home {
+        text-align: center;
+        padding: 30px 0 50px;
+        > a {
+            text-decoration: none;
+        }
+        > a, a:visited, a:hover, a:active {
+            color: inherit;
+        }
+    }
+</style>
